7a4eb8a59a4423d6b70a620344d51e64acc7865e
CrYuMovo <1418717236@qq.com>
2026-06-23 22:51:29 +0800

    chore: 应用.gitignore并清理已追踪的无关文件

---
+++ NEU-DET/train.py
@@ -0,0 +1,15 @@
+# train_steel.py （你自己创建的文件）
+from ultralytics import YOLO
+
+model = YOLO('yolov8n.pt')
+
+model.train(
+    data='data.yaml',      # 👈 指向你数据集根目录下的 data.yaml
+    epochs=50,
+    imgsz=416,
+    batch=8,
+    device='cpu',
+    workers=1,
+    project='runs_steel',
+    name='steel_v1'
+)
