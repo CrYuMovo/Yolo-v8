c84c58d16e4bcce2a96d776f620de18cb3ea7f66
UltralyticsAssistant <web@ultralytics.com>
2026-07-20 04:25:01 +0000

    Auto-format by https://ultralytics.com/actions

--- NEU-DET/train.py
+++ NEU-DET/train.py
@@ -1,14 +1,14 @@
 from ultralytics import YOLO
 
-if __name__ == '__main__':
-    model = YOLO('yolov8m.pt')
+if __name__ == "__main__":
+    model = YOLO("yolov8m.pt")
 
     results = model.train(
-        data='data.yaml',
+        data="data.yaml",
         epochs=100,
         imgsz=640,
         batch=8,
         device=0,
         amp=True,  # 保持你之前的设置
-        workers=4
+        workers=4,
     )
