c5d6c4eb7932819abdaa9427f3a1a42a530159fe
UltralyticsAssistant <web@ultralytics.com>
2026-07-28 04:12:17 +0000

    Auto-format by https://ultralytics.com/actions

--- yolov5_legacy/utils/torch_utils.py
+++ yolov5_legacy/utils/torch_utils.py
@@ -5,10 +5,10 @@ import time
 from copy import deepcopy
 
 import torch
-import torch.backends.cudnn as cudnn
-import torch.nn as nn
 import torch.nn.functional as F
 import torchvision
+from torch import nn
+from torch.backends import cudnn
 
 logger = logging.getLogger(__name__)
 
@@ -25,31 +25,33 @@ def init_torch_seeds(seed=0):
         cudnn.benchmark = True
 
 
-def select_device(device='', batch_size=None):
+def select_device(device="", batch_size=None):
     # device = 'cpu' or '0' or '0,1,2,3'
-    cpu_request = device.lower() == 'cpu'
+    cpu_request = device.lower() == "cpu"
     if device and not cpu_request:  # if device requested other than 'cpu'
-        os.environ['CUDA_VISIBLE_DEVICES'] = device  # set environment variable
-        assert torch.cuda.is_available(), 'CUDA unavailable, invalid device %s requested' % device  # check availablity
+        os.environ["CUDA_VISIBLE_DEVICES"] = device  # set environment variable
+        assert torch.cuda.is_available(), f"CUDA unavailable, invalid device {device} requested"  # check availability
 
     cuda = False if cpu_request else torch.cuda.is_available()
     if cuda:
-        c = 1024 ** 2  # bytes to MB
+        c = 1024**2  # bytes to MB
         ng = torch.cuda.device_count()
         if ng > 1 and batch_size:  # check that batch_size is compatible with device_count
-            assert batch_size % ng == 0, 'batch-size %g not multiple of GPU count %g' % (batch_size, ng)
+            assert batch_size % ng == 0, f"batch-size {batch_size:g} not multiple of GPU count {ng:g}"
         x = [torch.cuda.get_device_properties(i) for i in range(ng)]
-        s = 'Using CUDA '
-        for i in range(0, ng):
+        s = "Using CUDA "
+        for i in range(ng):
             if i == 1:
-                s = ' ' * len(s)
-            logger.info("%sdevice%g _CudaDeviceProperties(name='%s', total_memory=%dMB)" %
-                        (s, i, x[i].name, x[i].total_memory / c))
+                s = " " * len(s)
+            logger.info(
+                "%sdevice%g _CudaDeviceProperties(name='%s', total_memory=%dMB)"
+                % (s, i, x[i].name, x[i].total_memory / c)
+            )
     else:
-        logger.info('Using CPU')
+        logger.info("Using CPU")
 
-    logger.info('')  # skip a line
-    return torch.device('cuda:0' if cuda else 'cpu')
+    logger.info("")  # skip a line
+    return torch.device("cuda:0" if cuda else "cpu")
 
 
 def time_synchronized():
@@ -85,7 +87,7 @@ def find_modules(model, mclass=nn.Conv2d):
 
 def sparsity(model):
     # Return global model sparsity
-    a, b = 0., 0.
+    a, b = 0.0, 0.0
     for p in model.parameters():
         a += p.numel()
         b += (p == 0).sum()
@@ -94,26 +96,33 @@ def sparsity(model):
 
 def prune(model, amount=0.3):
     # Prune model to requested global sparsity
-    import torch.nn.utils.prune as prune
-    print('Pruning model... ', end='')
+    from torch.nn.utils import prune
+
+    print("Pruning model... ", end="")
     for name, m in model.named_modules():
         if isinstance(m, nn.Conv2d):
-            prune.l1_unstructured(m, name='weight', amount=amount)  # prune
-            prune.remove(m, 'weight')  # make permanent
-    print(' %.3g global sparsity' % sparsity(model))
+            prune.l1_unstructured(m, name="weight", amount=amount)  # prune
+            prune.remove(m, "weight")  # make permanent
+    print(f" {sparsity(model):.3g} global sparsity")
 
 
 def fuse_conv_and_bn(conv, bn):
     # Fuse convolution and batchnorm layers https://tehnokv.com/posts/fusing-batchnorm-and-conv/
 
     # init
-    fusedconv = nn.Conv2d(conv.in_channels,
-                          conv.out_channels,
-                          kernel_size=conv.kernel_size,
-                          stride=conv.stride,
-                          padding=conv.padding,
-                          groups=conv.groups,
-                          bias=True).requires_grad_(False).to(conv.weight.device)
+    fusedconv = (
+        nn.Conv2d(
+            conv.in_channels,
+            conv.out_channels,
+            kernel_size=conv.kernel_size,
+            stride=conv.stride,
+            padding=conv.padding,
+            groups=conv.groups,
+            bias=True,
+        )
+        .requires_grad_(False)
+        .to(conv.weight.device)
+    )
 
     # prepare filters
     w_conv = conv.weight.clone().view(conv.out_channels, -1)
@@ -133,24 +142,26 @@ def model_info(model, verbose=False):
     n_p = sum(x.numel() for x in model.parameters())  # number parameters
     n_g = sum(x.numel() for x in model.parameters() if x.requires_grad)  # number gradients
     if verbose:
-        print('%5s %40s %9s %12s %20s %10s %10s' % ('layer', 'name', 'gradient', 'parameters', 'shape', 'mu', 'sigma'))
+        print("%5s %40s %9s %12s %20s %10s %10s" % ("layer", "name", "gradient", "parameters", "shape", "mu", "sigma"))
         for i, (name, p) in enumerate(model.named_parameters()):
-            name = name.replace('module_list.', '')
-            print('%5g %40s %9s %12g %20s %10.3g %10.3g' %
-                  (i, name, p.requires_grad, p.numel(), list(p.shape), p.mean(), p.std()))
+            name = name.replace("module_list.", "")
+            print(
+                "%5g %40s %9s %12g %20s %10.3g %10.3g"
+                % (i, name, p.requires_grad, p.numel(), list(p.shape), p.mean(), p.std())
+            )
 
     try:  # FLOPS
         from thop import profile
-        flops = profile(deepcopy(model), inputs=(torch.zeros(1, 3, 64, 64),), verbose=False)[0] / 1E9 * 2
-        fs = ', %.1f GFLOPS' % (flops * 100)  # 640x640 FLOPS
+
+        flops = profile(deepcopy(model), inputs=(torch.zeros(1, 3, 64, 64),), verbose=False)[0] / 1e9 * 2
+        fs = ", %.1f GFLOPS" % (flops * 100)  # 640x640 FLOPS
     except:
-        fs = ''
+        fs = ""
 
-    logger.info(
-        'Model Summary: %g layers, %g parameters, %g gradients%s' % (len(list(model.parameters())), n_p, n_g, fs))
+    logger.info(f"Model Summary: {len(list(model.parameters())):g} layers, {n_p:g} parameters, {n_g:g} gradients{fs}")
 
 
-def load_classifier(name='resnet101', n=2):
+def load_classifier(name="resnet101", n=2):
     # Loads a pretrained model reshaped to n-class output
     model = torchvision.models.__dict__[name](pretrained=True)
 
@@ -176,7 +187,7 @@ def scale_img(img, ratio=1.0, same_shape=False):  # img(16,3,256,416), r=ratio
     else:
         h, w = img.shape[2:]
         s = (int(h * ratio), int(w * ratio))  # new size
-        img = F.interpolate(img, size=s, mode='bilinear', align_corners=False)  # resize
+        img = F.interpolate(img, size=s, mode="bilinear", align_corners=False)  # resize
         if not same_shape:  # pad/crop img
             gs = 32  # (pixels) grid size
             h, w = [math.ceil(x * ratio / gs) * gs for x in (h, w)]
@@ -186,20 +197,18 @@ def scale_img(img, ratio=1.0, same_shape=False):  # img(16,3,256,416), r=ratio
 def copy_attr(a, b, include=(), exclude=()):
     # Copy attributes from b to a, options to only include [...] and to exclude [...]
     for k, v in b.__dict__.items():
-        if (len(include) and k not in include) or k.startswith('_') or k in exclude:
+        if (len(include) and k not in include) or k.startswith("_") or k in exclude:
             continue
         else:
             setattr(a, k, v)
 
 
 class ModelEMA:
-    """ Model Exponential Moving Average from https://github.com/rwightman/pytorch-image-models
-    Keep a moving average of everything in the model state_dict (parameters and buffers).
-    This is intended to allow functionality like
-    https://www.tensorflow.org/api_docs/python/tf/train/ExponentialMovingAverage
-    A smoothed version of the weights is necessary for some training schemes to perform well.
-    This class is sensitive where it is initialized in the sequence of model init,
-    GPU assignment and distributed training wrappers.
+    """Model Exponential Moving Average from https://github.com/rwightman/pytorch-image-models Keep a moving average of
+    everything in the model state_dict (parameters and buffers). This is intended to allow functionality like
+    https://www.tensorflow.org/api_docs/python/tf/train/ExponentialMovingAverage A smoothed version of the weights
+    is necessary for some training schemes to perform well. This class is sensitive where it is initialized in the
+    sequence of model init, GPU assignment and distributed training wrappers.
     """
 
     def __init__(self, model, decay=0.9999, updates=0):
@@ -222,8 +231,8 @@ class ModelEMA:
             for k, v in self.ema.state_dict().items():
                 if v.dtype.is_floating_point:
                     v *= d
-                    v += (1. - d) * msd[k].detach()
+                    v += (1.0 - d) * msd[k].detach()
 
-    def update_attr(self, model, include=(), exclude=('process_group', 'reducer')):
+    def update_attr(self, model, include=(), exclude=("process_group", "reducer")):
         # Update EMA attributes
         copy_attr(self.ema, model, include, exclude)
